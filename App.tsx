b9d99c0b7b3164b0a487f7fbde1cefd5cf4e8cb3
juleni <69912179+juleni@users.noreply.github.com>
2022-08-17 18:21:00 +0200

    added chat/channels/...

--- App.tsx
+++ App.tsx
@@ -3,10 +3,18 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Text } from "react-native";
 import { StreamChat } from "stream-chat";
+import {
+  Channel,
+  ChannelList,
+  Chat,
+  MessageInput,
+  MessageList,
+  OverlayProvider,
+} from "stream-chat-expo";
 
 const API_KEY = "drudkatcayvd";
 const client = StreamChat.getInstance(API_KEY);
@@ -14,6 +22,9 @@ const client = StreamChat.getInstance(API_KEY);
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const [isReady, setIsReady] = useState(false);
+  const [selectedChannel, setSelectedChannel] = useState<any>(null);
 
   useEffect(() => {
     // connect the user only once within the app instance
@@ -30,11 +41,12 @@ export default function App() {
       console.log("User connected");
 
       // create a channel
-      const channel = client.channel("messaging", "JeeL", {
-        name: "JeeLsChannel",
+      const channel = client.channel("messaging", "JeeL3", {
+        name: "JeeLsChannel3",
       });
 
       await channel.watch(); // creates and updates channel
+      setIsReady(true);
     };
 
     connectUser();
@@ -43,12 +55,42 @@ export default function App() {
     return () => client.disconnectUser();
   }, []);
 
-  if (!isLoadingComplete) {
+  const onChannelPressed = (channel: any) => {
+    console.log("channel pressed: " + channel);
+    setSelectedChannel(channel);
+  };
+
+  if (!isLoadingComplete || !isReady) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <OverlayProvider>
+          <Chat client={client}>
+            {selectedChannel ? (
+              <Channel channel={selectedChannel}>
+                <MessageList />
+                <MessageInput />
+                <Text
+                  style={{ margin: 50 }}
+                  onPress={() => setSelectedChannel(null)}
+                >
+                  Go back
+                </Text>
+              </Channel>
+            ) : (
+              <Text style={{ margin: 50 }}>
+                {" "}
+                ***
+                <ChannelList
+                  onSelect={(channel) =>
+                    console.log("channel pressed: " + channel)
+                  }
+                />
+              </Text>
+            )}
+          </Chat>
+        </OverlayProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
